2ed33826ea5f430cbbd68344989777b54e29a7d5
pumidol-tt <pumidol.l@tektome.com>
2025-11-17 11:46:26 +0900

    fix: incorrect version

--- tektome/__init__.py
+++ tektome/__init__.py
@@ -1,6 +1,6 @@
 """Tektome Utils - Utilities for handling Tektome Resources."""
 
-__version__ = "0.1.0"
+__version__ = "0.3.1"
 
 from tektome.schema import BaseSchema, Resource, Resources, Project, Projects, AttributeDefinitions, Context, Date, DateTime
 
